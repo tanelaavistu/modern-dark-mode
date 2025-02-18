6c4893fa972b35ab6aac910052259c21c2c6bcd4
Tanel Aavistu <90567916+tanelaavistu@users.noreply.github.com>
2025-02-18 10:38:10 +0200

    Update header-script.js

--- header-script.js
+++ header-script.js
@@ -1,13 +1,14 @@
-<!-- Light/Dark Switch Header Script -->
+<!-- For Light Theme with Auto Option -->
 <script>
     (function() {
         let theme = localStorage.getItem('astrograaf_theme_pref');
-		
+	let defaultTheme = "auto";
+	
         if (!theme || (theme !== "light" && theme !== "dark" && theme !== "auto")) {
-            theme = "auto";
+            theme = defaultTheme;
             localStorage.setItem("astrograaf_theme_pref", theme);
         }
-		
+	
         document.documentElement.setAttribute("data-theme", theme);
     })();
 </script>
