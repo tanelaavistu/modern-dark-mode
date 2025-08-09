646e2ac24cf1cf42d5ef65b5294c98d7f7e0c8f2
Tanel Aavistu <90567916+tanelaavistu@users.noreply.github.com>
2025-08-09 23:37:27 +0300

    Update header-script.js

--- header-script.js
+++ header-script.js
@@ -2,8 +2,8 @@
 <script>
     (function() {
         let theme = localStorage.getItem('astrograaf_theme_pref');
-        if (theme == "light" || theme == "dark") {
-		document.documentElement.setAttribute("data-theme", theme);
+        if (theme) {
+			document.documentElement.setAttribute("data-theme", theme);
         }
     })();
 </script>
